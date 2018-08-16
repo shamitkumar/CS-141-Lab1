44f85b5aeff4bba6db1d3623c983a9122083dafb
shamitkr <42138795+shamitkr@users.noreply.github.com>
2018-08-16 20:39:17 +0530

    Add files via upload

---
+++ lab3q_2.cpp
@@ -0,0 +1,21 @@
+// library
+#include <iostream>
+using namespace std; 
+// write the main function
+int main(){
+int a,b,sum,difference,multiply,modulus;
+a=6;                          //giving values to variables
+b=2;			      
+sum=a+b;                       //doing sum
+difference=a-b;               //doing difference
+multiply=a*b;                  //doing multiply
+float division=(a/b);         //doing division
+modulus=(a%b);                //doing modulus
+cout<<sum<<endl;              //sum will be performed and line will be changed
+cout<<difference<<endl;       //difference will be performed and line will be changed
+cout<<multiply<<endl;         //multiply will be performed and line will be changed
+cout<<division<<endl;         //division will be performed and line will be changed
+cout<<modulus<<endl;          //modulus will be performed and line will be changed
+
+return 0;
+}
