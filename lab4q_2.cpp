8709c36ee17caf111e05d91bcf9bacc5952a6678
shamitkr <42138795+shamitkr@users.noreply.github.com>
2018-08-22 16:56:43 +0530

    Add files via upload

---
+++ lab4q_2.cpp
@@ -0,0 +1,18 @@
+//program to enter a temperature in celcius and converting it into fahrenheit
+//include the library
+#include <iostream>
+using namespace std;
+//including the function
+int main() {
+//declaring the variables
+float b;
+//asking for a value
+cout <<"Please enter a temperature in celcius:" ;
+//reading the variables 
+cin >> b;
+cout <<" The value you entered is " << b;
+//performing the calculation
+cout <<" and its value in fahrenheit is " <<(b*1.0)+32 <<"c" << ".\n";
+return 0;
+}
+
