7d9a4abde582654a46bf3a141ed187da4c460441
shamitkr <42138795+shamitkr@users.noreply.github.com>
2018-08-22 16:57:34 +0530

    Add files via upload

---
+++ lab4q__5.cpp
@@ -0,0 +1,18 @@
+//program to enter two angles and find the third angle
+//include the library
+# include<iostream>
+using namespace std;
+//including function
+int main() {
+//declaring variables
+int angle_a;
+int angle_b;
+//asking for an input value 
+cout << "Enter your first angle" ;
+cin >> angle a ;
+cout << " Enter your second angle" ;
+cin >> angle _b;
+//reading the variables
+cout <<" Hence your third angle is" << 180-(angle_a+angle_b)<< ".\n";
+return 0;
+}
