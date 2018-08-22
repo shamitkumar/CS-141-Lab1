b35e5999f504922e1c408cd4a3ad002bcde3e639
shamitkr <42138795+shamitkr@users.noreply.github.com>
2018-08-22 16:58:06 +0530

    Add files via upload

---
+++ lab4q_6.cpp
@@ -0,0 +1,19 @@
+//program to enter base and height of a triangle and using that to find its area
+//include the library
+#include <iostream>
+using namespace std;
+//including function
+int main() {
+float base;
+float height;
+//asking for input 
+cout <<"Enter the base of the triangle" ;
+cin >> base;
+cout <<"Enter the height of the triangle" ;
+cin >> height;
+//doing the calculation
+cout <<"Hence the area of the triangle is" <<(0.5*base)*(height)<<".\n";
+
+return 0;
+}
+
