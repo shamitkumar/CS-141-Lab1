7177dff3a1114fbc1050663b4d8409543e8df91a
shamitkr <42138795+shamitkr@users.noreply.github.com>
2018-09-13 02:45:51 +0530

    Add files via upload

---
+++ lab6q_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int byvalue(int a,int b)
+{int c= a+b;
+return c;}
+
+void byreference(int a,int b,int &sum)
+{sum= byvalue(a,b);
+cout<<sum;}
+
+int main(){
+int a,b,sum=0;
+cout<<"enter the numbers"<<endl;
+cin>>a>>b;
+byvalue(a,b);
+byreference(a,b,sum);
+return 0;}
---
+++ lab6q_4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int byvalue(int a,int b)
+{if(a<b)
+return a;
+else 
+return b; }
+
+void byreference(int a,int b,int &sum)
+{sum= byvalue(a,b);
+cout<<"minimum="<<sum;}
+
+int main(){
+int a,b,sum=0;
+cout<<"enter the numbers"<<endl;
+cin>>a>>b;
+byvalue(a,b);
+byreference(a,b,sum);
+return 2312;}
